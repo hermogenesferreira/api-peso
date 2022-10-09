daf604ac6ffec2b76f8a998912c27d38fdabc1ca
Hermogenes <hermogenes.ferreira@gmail.com>
2022-10-09 14:28:44 -0300

    add media

--- src/controllers/CowController.js
+++ src/controllers/CowController.js
@@ -16,7 +16,7 @@ class CowController {
   async showMedia(req, res) {
     try {
       const { id } = req.params;
-      const weighing = await conn.query(`SELECT AVG(weighings.value) FROM weighings AS media WHERE weighings.cowId = ${id}`);
+      const weighing = await conn.query(`SELECT AVG(weighings.value) FROM weighings AS media WHERE weighings.cowId = 1`);
       res.status(200).json({ weighing });
     } catch (err) {
       return res.json(500).json(err);
